f0a66f94e9001adddfc51c5feadcfcdbad679a12
Hattho <32792626+Hattho@users.noreply.github.com>
2017-11-11 14:47:12 +0200

    Lab5-2-3

--- Laborator 5/L5-2-3.cpp
+++ Laborator 5/L5-2-3.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int n;
-int i,j;
+int i,j,k;
 
 int main () {
     cin>>n;
+    k=0;
+    for(i=1;i<=n;i++){
+        if(k==n)
+            break;
 
-    for(i=1;i<=n;i++)
-        for(j=1;j<=i;j++)
-            cout<<j<<" ";
-
+        for(j=1;j<=i;j++){
+                k++;
+                cout<<j<<" ";
+                if(k==n)
+                    break;
+        }
+    }
     return 0;
 
 }
