00cd0f4c899913a73151c165cd258279dc1db0bb
Hattho <32792626+Hattho@users.noreply.github.com>
2018-01-16 21:41:45 +0200

    L1-1-2

---
+++ Laborator 1/L1-1-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define multiply(x, y) x*y;
+
+int x,y;
+
+int main () {
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    int p=multiply(x, y)
+
+    printf("%d ", p);
+
+    return 0;
+
+}
+
